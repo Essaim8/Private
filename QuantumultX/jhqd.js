9b9fda3d7cd052afd885897244201e30db276ca0
Essaim8 <127705161+Essaim8@users.noreply.github.com>
2023-07-16 23:18:28 +0800

    Update jhqd.js

--- QuantumultX/jhqd.js
+++ QuantumultX/jhqd.js
@@ -1,16 +1,16 @@
 /**************************************
 作者:Zoo
-日期:2023.07.14
+日期:2023.07.16
 使用教程:
 1.复制Cookie脚本到重写
 2.复制[task_local]内容到本地
-3.圈x开抓包手动签到一次，然后关闭抓包搜https://yunbusiness.ccb.com/clp_coupon/txCtrl?txcode=A3341A040，把请求体内容全部复制到url.body='{复制到这个里面}' 然后保存到本地就可以了
+3.圈x开抓包手动签到一次，然后关闭抓包搜https://yunbusiness.ccb.com/clp_coupon/txCtrl?txcode=A3341A040，把请求体文本查看，内容全部复制到url.body='{复制到这个里面}' 然后保存到本地就可以了
 4.关闭Cookie脚本
 [rewrite_local]
-^https:\/\/yunbusiness\.ccb\.com\/clp_coupon\/txCtrl\?txcode\=A3341A040 url script-request-header https://raw.githubusercontent.com/Crazy-Z7/Task/main/jhshCookie.js
+^https:\/\/yunbusiness\.ccb\.com\/clp_coupon\/txCtrl\?txcode\=A3341A040 url script-request-header https://raw.githubusercontent.com/Crazy-Z7/Task/main/JhshCookie.js
 
 [task_local]
-40 8 * * * https://raw.githubusercontent.com/Crazy-Z7/Task/main/jhsh.js, tag=建行生活积分签到,enabled=true
+40 8 * * * https://raw.githubusercontent.com/Crazy-Z7/Task/main/Jhsh.js, tag=建行生活积分签到,enabled=true
 [MITM]
 hostname = yunbusiness.ccb.com
 *****************************************/
@@ -25,27 +25,21 @@ const signheaderVal = photonmang.getdata(signheaderKey)
 sign()
 function sign() {
   const url = { url: `https://yunbusiness.ccb.com/clp_coupon/txCtrl?txcode=A3341A040`, headers: JSON.parse(signheaderVal) }
-  //请求体内容复制到下方括号里面over!
+  //请求体内容用文本查看，然后复制到下方括号里面，不要把文本里面的括号也复制了over!
     url.body = '{"ACT_ID":"20230628070000000001","MEB_ID":"YSM202206161116002","USR_TEL":"17351930836","REGION_CODE":"321000","chnlType":"1","regionCode":"321000"}'
   photonmang.post(url, (error, response, data) => {
-    photonmang.log(`${cookieName}, data: ${data}`)
-    const title = `${cookieName}`
-    let subTitle = ''
-    let detail = ''
+   const title = `${cookieName}`
+  let subTitle = ''
     const result = JSON.parse(data)
-    if (result.Code == 1) {
+    if (result.errCode == 0) {
       subTitle = `签到结果: 签到成功`
-      
-    } else if (result.Code == 0) {
-      subTitle = `签到结果: ${result.Message}`
+    } else {
+      subTitle = `签到失败❌: ${result.errMsg}`
     } 
-    photonmang.msg(title, subTitle)
+    photonmang.msg(title, subTitle,)
     photonmang.done()
   })
 }
-
-
-
 function init() {
   isSurge = () => {
     return undefined === this.$httpClient ? false : true
